02dbad2b0c1e13c4851cfd0fb7a4526f150dd79c
AngeloC3 <cataldo.an@northeastern.edu>
2024-12-01 16:04:51 -0500

    a6

--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -1,31 +1,34 @@
 import * as dao from "./dao.js";
-import * as coursesDao from '../Courses/dao.js';
 
 export default function EnrollmentRoutes(app) {
-  const enrollUserInCourse = (req, res) => { 
-    const { courseId } = req.params;
-    const { userId } = req.body;
-    const course = coursesDao.findEnrolledUserCourse(userId, courseId)
-    if (course) {
-        res.sendStatus(401);
-        return;
+  const enrollUserInCourse = async (req, res) => {
+    const { uid } = req.body;
+    let { cid } = req.params;
+    if (uid === "current") {
+      const currentUser = req.session["currentUser"];
+      uid = currentUser._id;
     }
-    dao.enrollUserInCourse(userId, courseId)
+    await dao.enrollUserInCourse(uid, cid);
     res.sendStatus(200);
   };
+ 
 
-  const unenrollUserInCourse = (req, res) => { 
-    const { courseId, userId } = req.params;
-    dao.unenrollUserInCourse(courseId, userId)
+  const unenrollUserInCourse = async(req, res) => { 
+    let { uid, cid } = req.params;
+    if (uid === "current") {
+      const currentUser = req.session["currentUser"];
+      uid = currentUser._id;
+    }
+    await dao.unenrollUserFromCourse(uid, cid);
     res.sendStatus(200);
   };
 
 
   app.get("/api/enrollments", (req, res) => {
     const enrollments = dao.findAllEnrollments();
-    res.send(enrollments);
+    res.json(enrollments);
   });
 
-  app.post("/api/enrollments/enroll/:courseId/", enrollUserInCourse);
-  app.post("/api/enrollments/unenroll/:courseId/:userId", unenrollUserInCourse);
+  app.post("/api/enrollments/enroll/:cid/", enrollUserInCourse);
+  app.post("/api/enrollments/unenroll/:cid/:uid", unenrollUserInCourse);
 }
